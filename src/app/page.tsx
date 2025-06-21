0c11596d5fa1b98af43a45563285578e42fd3c33
Steven Shelestowsky <shelesteven@gmail.com>
2025-06-21 07:30:42 -0400

    Sign in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const formSchema = z.object({
     email: z.string().email(),
 });
 
-export default function Home() {
+export default function Page() {
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -28,8 +28,8 @@ export default function Home() {
         <section className="flex grow items-center justify-center bg-gradient-to-br from-indigo-200 to-white dark:from-indigo-950/40 dark:to-gray-950 px-6 py-16 overflow-hidden">
             <div className="mx-auto flex flex-col items-center justify-between z-10">
                 <div className="text-center max-w-2xl">
-                    <h1 className="text-5xl md:text-6xl font-extrabold text-indigo-500 dark:text-indigo-400 leading-tight mb-2">Group up. Save big.</h1>
-                    <p className="text-lg md:text-2xl text-gray-800 dark:text-gray-200 mb-8">Discover smarter shopping. Create or join a group buy to save money on bulk items and shipping.</p>
+                    <h1 className="text-6xl md:text-7xl font-extrabold text-indigo-500 dark:text-indigo-400 leading-tight mb-2">Group up. Save big.</h1>
+                    <p className="text-md md:text-lg text-gray-800 dark:text-gray-200 mb-8">Discover smarter shopping. Create or join a group buy to save money on bulk items and shipping.</p>
                 </div>
 
                 <Card className="w-full max-w-md bg-background/80 p-8 shadow-xl">
